096076a07ad9b163bca56707980dab31132a5abb
Abhinav Tushar <lepisma@fastmail.com>
2018-04-03 18:58:21 -0400

    Hide internal fns

--- ledge/merge.py
+++ ledge/merge.py
@@ -11,19 +11,19 @@ from ledge.datatypes import Truth, Loss
 Series = Union[Truth, Loss]
 
 
-def get_right_envelope(ds: xr.Dataset) -> xr.DataArray:
+def _get_right_envelope(ds: xr.Dataset) -> xr.DataArray:
     """
     Get an envelope of non nan values from the right
     """
 
-    array = mat.to_array()
+    array = ds.to_array()
     n_cols = array.shape[0]
 
     col_idx = n_cols - 1 - np.flip(np.isnan(array), axis=0).argmin(dim="variable")
     return array.isel(variable = col_idx)
 
 
-def get_lag(series: Series) -> int:
+def _get_lag(series: Series) -> int:
     """
     Parse lag value from the series
     """
@@ -31,22 +31,22 @@ def get_lag(series: Series) -> int:
     return ("lag" in series.attrs and series.attrs["lag"]) or np.inf
 
 
-def sort_lags(series_list: List[Series]) -> List[Series]:
+def _sort_lags(series_list: List[Series]) -> List[Series]:
     """
     Sort series according to lags. Latest goes to the end.
     """
 
-    return sorted(series_list, key=get_lag)
+    return sorted(series_list, key=_get_lag)
 
 
-def merge_lags(series_list: List[Series]) -> xr.Dataset:
+def _merge_lags(series_list: List[Series]) -> xr.Dataset:
     """
     Create a left joined dataset
     """
 
     # Add dummy names based on lags
     for series in series_list:
-        series.name = get_lag(series)
+        series.name = _get_lag(series)
 
     return xr.merge(series_list, join="left")
 
@@ -56,8 +56,8 @@ def latest(series_list: List[Series]) -> Series:
     Skip older lag values. Prefer series without lag set.
     """
 
-    dataset = merge_lags(sort_lags(series_list))
-    return get_right_envelope(dataset)
+    dataset = _merge_lags(_sort_lags(series_list))
+    return _get_right_envelope(dataset)
 
 
 def mix_alpha(series_list: List[Series]) -> Series:
