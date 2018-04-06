bece1e9cf36d094eb0255f464c3901e79cb63d75
Abhinav Tushar <lepisma@fastmail.com>
2018-04-05 20:20:34 -0400

    Test for merge latest

---
+++ tests/test_merge.py
@@ -0,0 +1,17 @@
+import xarray as xr
+import numpy as np
+from ledge import merge
+
+
+def test_latest():
+    times = 7
+    lags = 4
+
+    series_list = [
+        xr.DataArray(np.ones((times - lag,)) * lag, attrs={ "lag": lag }, dims="timepoints", coords={ "timepoints" : range(times - lag) })
+        for lag in range(lags)
+    ]
+
+    expected = xr.DataArray([3, 3, 3, 3, 2, 1, 0], dims="timepoints", coords={ "timepoints": range(times) })
+
+    assert np.all(expected, merge.latest(series_list))
