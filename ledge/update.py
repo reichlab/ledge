c8cc3ab15d388c24e9404612dca8754796cfa69c
Abhinav Tushar <lepisma@fastmail.com>
2018-04-07 18:18:36 -0400

    mw implementation

--- ledge/update.py
+++ ledge/update.py
@@ -8,6 +8,15 @@ from typing import List
 from ledge.datatypes import Loss, Weight
 
 
+def _uniform_weights(models: List[string], ones=True) -> Weight:
+    weights = xr.DataArray(np.ones(len(models)))
+
+    if not ones:
+        weights /= len(models)
+
+    return xr.DataArray(weights, dims="model", coords={ "model": models })
+
+
 def ftl(losses: List[Loss], init_weights=None) -> Weight:
     """
     Follow the leader update. Give full weight to the model with least loss.
@@ -15,9 +24,9 @@ def ftl(losses: List[Loss], init_weights=None) -> Weight:
 
     models = [loss.attrs["model"] for loss in losses]
     best_idx = np.argmin([loss.sum() for loss in losses])
-    weights = [1 if i == best_idx else 0 for i in range(len(losses))]
 
-    return xr.DataArray(weights, dims="model", coords={ "model": models })
+    return xr.DataArray([1 if i == best_idx else 0 for i in range(len(losses))],
+                        dims="model", coords={ "model": models })
 
 
 def ftpl(losses: List[Loss]) -> Weight:
@@ -48,7 +57,14 @@ def mw(losses: List[Loss], eta: float, init_weights=None) -> Weight:
     Multiplicative weight update. :math:`w_i(t + 1) = w_i(t) (1 - \eta m_i(t))`
     """
 
-    raise NotImplementedError()
+    models = [loss.attrs["model"] for loss in losses]
+
+    if init_weights is None:
+        init_weights = _uniform_weights(models)
+
+    updates = [(1 - eta * loss).prod() for loss in losses]
+
+    return init_weights * updates
 
 
 def hedge(losses: List[Loss], eta: float, init_weights=None) -> Weight:
