c524345058bc8b9c34bf1dd5b896943daa7ae621
Abhinav Tushar <lepisma@fastmail.com>
2018-04-15 22:04:55 -0400

    Window based filling functions

--- ledge/fill.py
+++ ledge/fill.py
@@ -3,16 +3,76 @@ Imputation functions for dataset
 """
 
 import xarray as xr
+import numpy as np
 from typing import List, Union
+from functools import partial, wraps
 from ledge.datatypes import Truth, Loss
 
 
 Series = Union[Truth, Loss]
 
 
-def diff_mean(series_list: List[Series], incremental=False) -> List[Series]:
+def window_linear(size, alpha=1):
     """
-    Fill in the series using mean of the diff between a `base` series.
+    Return arithmetically increasing weights.
+    Value of alpha doesn't matter if using normalization.
+    """
+
+    return np.linspace(alpha, alpha * size, size)
+
+
+def window_geometric(size, gamma):
+    """
+    Geometrically increasing weights
+    """
+
+    return np.geomspace(gamma, gamma ** size, num=size)[::-1]
+
+
+def window_uniform(size):
+    """
+    Uniform weights
+    """
+
+    return np.ones(size)
+
+
+def normalize(window_fn):
+    """
+    Decorator for normalizing the window function output
+    """
+
+    @wraps(window_fn)
+    def _wrapper(*args, **kwargs):
+        weights = window_fn(*args, **kwargs)
+        return weights / np.sum(weights)
+
+    return _wrapper
+
+
+def lookback(window_size):
+    """
+    Decorator for clipping the lookback size to a limit.
+    """
+
+    def lookback_dec(window_fn):
+        @wraps(window_fn)
+        def _wrapper(size, *args, **kwargs):
+            if window_size is None:
+                return window_fn(size, *args, **kwargs)
+            else:
+                if window_size < size:
+                    pad_len = size - window_size
+                    return np.pad(window_fn(window_size, *args, **kwargs), (pad_len, 0), "constant")
+                else:
+                    return window_fn(size, **kwargs)
+        return _wrapper
+    return lookback_dec
+
+
+def diff_window(series_list: List[Series], window_fn, inc=False) -> List[Series]:
+    """
+    Fill in the series using weighted mean of the diff between a `base` series.
     If incremental is true, the base series is the previous series. If false,
     the base is the first series.
     """
@@ -24,12 +84,33 @@ def diff_mean(series_list: List[Series], incremental=False) -> List[Series]:
     output = [base_series]
 
     for i in range(1, len(series_list)):
-        if incremental:
+        if inc:
             base_series = output[i - 1]
 
-        diff = (series_list[i] - base_series).mean()
-        fallback = base_series + diff
+        diffs = series_list[i] - base_series
+        fallback = base_series + np.dot(diffs, window_fn(len(diffs)))
 
         output.append(series_list[i].combine_first(fallback))
 
     return output
+
+
+"""
+Weigh the past by taking mean of all
+"""
+diff_mean = partial(diff_window, window_fn=normalize(window_uniform))
+
+
+"""
+Fill in values by weighing the past linearly
+"""
+diff_linear = partial(diff_window, window_fn=normalize(window_linear))
+
+
+def diff_geometric(series_list: List[Series], gamma: float, inc=False) -> List[Series]:
+    """
+    Fill in values by weighing the past geometrically using gamma
+    """
+
+    window_fn = normalize(partial(window_geometric, gamma=gamma))
+    return diff_window(series_list, window_fn=window_fn, inc=inc)
--- tests/test_fill.py
+++ tests/test_fill.py
@@ -3,27 +3,25 @@ import numpy as np
 from ledge import fill
 from utils import make_series
 
+TIMES = 10
+INPUT_LIST = [
+    make_series(np.zeros(TIMES), range(TIMES), 0),
+    make_series(np.linspace(0, TIMES - 3, TIMES - 2), range(TIMES - 2), 1),
+    make_series(np.linspace(1, TIMES - 3, TIMES - 3), range(TIMES - 3), 2)
+]
 
 def test_diff_mean_noninc():
     """
     Check if the non incremental version works fine
     """
 
-    times = 10
-
-    input_list = [
-        make_series(np.zeros(times), range(times), 0),
-        make_series(np.linspace(0, times - 3, times - 2), range(times - 2), 1),
-        make_series(np.linspace(1, times - 3, times - 3), range(times - 3), 2)
-    ]
-
     expected_output_list = [
-        make_series(np.zeros(times), range(times), 0),
-        make_series(np.concatenate([np.linspace(0, times - 3, times - 2), [3.5] * 2]), range(times), 1),
-        make_series(np.concatenate([np.linspace(1, times - 3, times - 3), [4.0] * 3]), range(times), 2),
+        make_series(np.zeros(TIMES), range(TIMES), 0),
+        make_series(np.concatenate([np.linspace(0, TIMES - 3, TIMES - 2), [3.5] * 2]), range(TIMES), 1),
+        make_series(np.concatenate([np.linspace(1, TIMES - 3, TIMES - 3), [4.0] * 3]), range(TIMES), 2),
     ]
 
-    output = fill.diff_mean(input_list)
+    output = fill.diff_mean(INPUT_LIST)
 
     for o, eo in zip(output, expected_output_list):
         assert np.allclose(o, eo)
@@ -34,21 +32,41 @@ def test_diff_mean_inc():
     Check for the incremental version
     """
 
-    times = 10
-
-    input_list = [
-        make_series(np.zeros(times), range(times), 0),
-        make_series(np.linspace(0, times - 3, times - 2), range(times - 2), 1),
-        make_series(np.linspace(1, times - 3, times - 3), range(times - 3), 2)
-    ]
-
     expected_output_list = [
-        make_series(np.zeros(times), range(times), 0),
-        make_series(np.concatenate([np.linspace(0, times - 3, times - 2), [3.5] * 2]), range(times), 1),
-        make_series(np.concatenate([np.linspace(1, times - 3, times - 3), [8, 4.5, 4.5]]), range(times), 2),
+        make_series(np.zeros(TIMES), range(TIMES), 0),
+        make_series(np.concatenate([np.linspace(0, TIMES - 3, TIMES - 2), [3.5] * 2]), range(TIMES), 1),
+        make_series(np.concatenate([np.linspace(1, TIMES - 3, TIMES - 3), [8, 4.5, 4.5]]), range(TIMES), 2),
     ]
 
-    output = fill.diff_mean(input_list, inc=True)
+    output = fill.diff_mean(INPUT_LIST, inc=True)
+
+    for o, eo in zip(output, expected_output_list):
+        assert np.allclose(o, eo)
+
+
+def test_diff_linear_noninc():
+    expected_output_list = [
+        make_series(np.zeros(TIMES), range(TIMES), 0),
+        make_series(np.concatenate([np.linspace(0, TIMES - 3, TIMES - 2), [4.6666666666666661] * 2]), range(TIMES), 1),
+        make_series(np.concatenate([np.linspace(1, TIMES - 3, TIMES - 3), [5.0] * 3]), range(TIMES), 2),
+    ]
+
+    output = fill.diff_linear(INPUT_LIST)
+
+    for o, eo in zip(output, expected_output_list):
+        assert np.allclose(o, eo)
+
+
+def test_diff_linear_inc():
+    expected_output_list = [
+        make_series(np.zeros(TIMES), range(TIMES), 0),
+        make_series(np.concatenate([np.linspace(0, TIMES - 3, TIMES - 2), [4.6666666666666661] * 2]), range(TIMES), 1),
+        make_series(np.concatenate([np.linspace(1, TIMES - 3, TIMES - 3), [8, 5.6666666666666661, 5.6666666666666661]]), range(TIMES), 2),
+    ]
+
+    output = fill.diff_linear(INPUT_LIST, inc=True)
+    print(output)
+    print(expected_output_list)
 
     for o, eo in zip(output, expected_output_list):
         assert np.allclose(o, eo)
